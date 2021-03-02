d4b54a4c39f33d7b7e163e43338f1b1e177b1091
Yönet <3523671+Yonet@users.noreply.github.com>
2021-03-02 15:54:38 -0800

    Update environment.dev.ts

--- projects/health-portal/src/environments/environment.dev.ts
+++ projects/health-portal/src/environments/environment.dev.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   cognitive: {
-    key: '8b075433686142f8b6af1e65782179cd',
-    endPoint: 'https://ignitecogserv.cognitiveservices.azure.com/',
-    location: 'westus2'
+    key: 'your key here',
+    endPoint: 'your end point here',
+    location: 'your location here'
   },
   speech: {
-    key: '112283abd0b9429db9bcb76b5047462d',
-    endPoint: 'https://centralus.api.cognitive.microsoft.com/sts/v1.0/issuetoken',
-    location: 'centralus'
+    key: 'your key here',
+    endPoint: 'your end point here',
+    location: 'your location here'
   }
 };
